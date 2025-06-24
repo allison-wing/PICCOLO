570b85cecb8c686182cb99315b76231cec6d0e27
Allison Wing <awing@fsu.edu>
2025-06-24 17:55:04 -0400

    add more timing increments

--- process_imerg_finalrun.py
+++ process_imerg_finalrun.py
@@ -8,20 +8,34 @@ import glob
 import os
 import time
 
+yy = 2000
+
 start = time.time()
 
 # Load IMERG data (30-minute data, 0.1 degree resolution)
-file_paths = glob.glob("/huracan/tank4/cornell/ORCESTRA/imerg/final_run_V07_TropAtl_AugSep/3B-HHR.MS.MRG.3IMERG.2021*")
+file_paths = glob.glob("/huracan/tank4/cornell/ORCESTRA/imerg/final_run_V07_TropAtl_AugSep/3B-HHR.MS.MRG.3IMERG." + str(yy) + "*")
 
+end = time.time()
+print("Elapsed time for loading file paths:", end - start, "seconds")
+
+start = time.time()
 #Open the files and combine them into a single dataset
 #IMERG_ds = xr.open_mfdataset(file_paths,engine='h5netcdf',group='Grid',combine='by_coords')
 IMERG_ds = xr.open_mfdataset(file_paths,combine='by_coords')
 
+end = time.time()
+print("Elapsed time for opening dataset:", end - start, "seconds")
+
+start = time.time()
 # Swap lat & lon dimensions, so it is (time,lat,lon)
 IMERG = IMERG_ds.transpose('time','lat','lon',...)
 
+end = time.time()
+print("Elapsed time for taking transpose:", end - start, "seconds")
+
+start = time.time()
 #Write out to new netcdf file
-IMERG.to_netcdf('/huracan/tank4/cornell/ORCESTRA/imerg/imerg_finalrun_20210809.nc')
+IMERG.to_netcdf('/huracan/tank4/cornell/ORCESTRA/imerg/imerg_finalrun_' + str(yy) + '0809.nc')
 
 end = time.time()
-print("Elapsed time for processing IMERG data:", end - start, "seconds")
+print("Elapsed time for writing to file:", end - start, "seconds")
