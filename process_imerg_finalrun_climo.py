bb02553b96981d605c3819777d52fac9447681f0
Allison Wing <awing@fsu.edu>
2025-08-26 23:11:47 -0400

    tweaking colorbar, adding a few years

--- process_imerg_finalrun_climo.py
+++ process_imerg_finalrun_climo.py
@@ -10,6 +10,7 @@ import numpy as np
 import glob
 import os
 import time
+import gc
 
 start = time.time()
 
@@ -33,7 +34,7 @@ end_time = '09-'+end_day+'T00:00:00'
 filebase = "/huracan/tank4/cornell/ORCESTRA/imerg/final_run_V07_TropAtl_AugSep/3B-HHR.MS.MRG.3IMERG."
 
 # For each year, extract for time period of campaign and region
-years = np.arange(1999,2022)  # 1999-2021
+years = np.arange(1998,2024)  # 1998-2023
 for yy in years:
     # Load IMERG data
     file_paths = glob.glob(filebase + str(yy) + "*")
@@ -44,28 +45,42 @@ for yy in years:
     #Open the files and combine them into a single dataset
     IMERG_ds = xr.open_mfdataset(file_paths,combine='by_coords')
 
+    # Load into memory
+    IMERG_ds.load()
+
     # Swap lat & lon dimensions, so it is (time,lat,lon)
     IMERG = IMERG_ds.transpose('time','lat','lon',...)
+    
+    # Close dataset
+    IMERG_ds.close()
     
     # Select region and time period of interest
     IMERGyear = IMERG.sel(lat=slice(latMin,latMax),lon=slice(lonMin,lonMax),time=slice(str(yy)+'-'+start_time, str(yy)+'-'+end_time)).mean(dim='time')
     zonalmean_precip_year = IMERGyear.sel(lon=slice(lon1,lon2)).mean(dim='lon')
-    if yy == 1999:
+    if yy == years[0]:
         IMERGCampaign = IMERGyear
         zonalmean_precip = zonalmean_precip_year
     else:
         IMERGCampaign = xr.concat([IMERGCampaign,IMERGyear],dim='time')
         zonalmean_precip = xr.concat([zonalmean_precip,zonalmean_precip_year],dim='time')
+        
+    del IMERG
+    del IMERGyear
+    gc.collect()
+    
+    end = time.time()
+    print("Elapsed time for processing one year of IMERG data:", end - start, "seconds")
 
+start = time.time()
 #Take climatological mean over campaign time period over all years
 IMERGClimoMean = IMERGCampaign.mean(dim='time')
 
 #Save to netcdf file (climatological mean at each grid point over campaign time period in all years)
-IMERGClimoMean.to_netcdf('/huracan/tank4/cornell/ORCESTRA/imerg/imerg_campaign_climo.nc')
+IMERGClimoMean.to_netcdf('/huracan/tank4/cornell/ORCESTRA/imerg/imerg_campaign_climo_1998_2023.nc')
 
 # Save zonal mean to netcdf file (zonal mean over campaign time period in each year)
 zonalmean_precip = zonalmean_precip.assign_coords(time=years)
-zonalmean_precip.to_netcdf('/huracan/tank4/cornell/ORCESTRA/imerg/imerg_zonalmean_precip.nc')
+zonalmean_precip.to_netcdf('/huracan/tank4/cornell/ORCESTRA/imerg/imerg_zonalmean_precip_1998_2023.nc')
 
 end = time.time()
-print("Elapsed time for processing IMERG data:", end - start, "seconds")
+print("Elapsed time for taking climo mean and writing out", end - start, "seconds")
